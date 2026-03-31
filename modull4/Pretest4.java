d30a7f85e22fdf6d525f41ad48e84cb45b1cd31b
alifafatin <fatinramadhanialifa@gmail.com>
2026-03-31 14:42:04 +0700

    pretest

---
+++ modull4/Pretest4.java
@@ -0,0 +1,13 @@
+package modull4; 
+
+class mahasiswa { 
+    String nama;
+    int umur;
+    String jurusan;
+}
+
+public class Pretest4 {
+    public static void main(String[] args) {
+        mahasiswa mhsBaru = new mahasiswa(); 
+    } 
+}
